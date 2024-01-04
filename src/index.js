c34d60c47dcab7dbf5098ce22a0cd54186223576
gnoparus <surapong@gmail.com>
2024-01-04 22:13:06 +0700

    use cf ai gateway

--- src/index.js
+++ src/index.js
@@ -8,7 +8,11 @@ app.get('/', async c => {
 
 	const question = c.req.query("question") || "What is the meaning of love?";
 	console.log("question", question)
-	const model = new ChatOpenAI({ openAIApiKey: c.env.OPENAI_API_KEY, modelName: "gpt-3.5-turbo" });
+	const model = new ChatOpenAI({
+		configuration: { baseURL: c.env.GATEWAY_URL },
+		openAIApiKey: c.env.OPENAI_API_KEY,
+		modelName: "gpt-3.5-turbo"
+	});
 	const prompt = PromptTemplate.fromTemplate(`You are helpful assistant. Question: {question}`)
 	console.log("prompt", prompt)
 
